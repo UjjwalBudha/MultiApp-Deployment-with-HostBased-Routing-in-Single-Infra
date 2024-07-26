61a15b79fb19c984ef64f39f5f9f59473e981bda
Ujwal budha <ujjwal.budha07@gmail.com>
2024-07-26 13:27:22 +0545

    updated scripts

--- backend/backend-2/index.js
+++ backend/backend-2/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const mysql = require("mysql2");
-const path = require('path');
-const cors = require('cors');
-const multer = require('multer');
-const fs = require('fs');
-require('dotenv').config();
+const path = require("path");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+require("dotenv").config();
 
 // Create a connection to the database
 const db = mysql.createConnection({
@@ -31,10 +31,10 @@ db.on("error", (err) => {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads');
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
-    console.log(req.body, 'in');
+    console.log(req.body, "in");
     cb(null, `${req.body.productId}${path.extname(file.originalname)}`);
   },
 });
@@ -44,9 +44,9 @@ const upload = multer({ storage: storage });
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.post('/thumbnailUpload', upload.single('productThumbnail'), (req, res) => {
+app.post("/thumbnailUpload", upload.single("productThumbnail"), (req, res) => {
   try {
     console.log(req.file);
     return res.json({ data: req.file.filename });
@@ -55,20 +55,19 @@ app.post('/thumbnailUpload', upload.single('productThumbnail'), (req, res) => {
   }
 });
 
-app.get('/products01', (req, res) => {
-  const q = 'select * from product';
+app.get("/products01", (req, res) => {
+  const q = "select * from product";
   db.query(q, (err, data) => {
     console.log(err, data);
     if (err) return res.json({ error: err.sqlMessage });
     else return res.json({ data });
   });
 });
-
-app.post('/products01', (req, res) => {
+app.post("/products01", (req, res) => {
   const q = `insert into product(productId, productTitle, productDescription, productPrice, availableQuantity, productThumbnail)
       values(?)`;
   const values = [...Object.values(req.body)];
-  console.log('insert', values);
+  console.log("insert", values);
   db.query(q, [values], (err, data) => {
     console.log(err, data);
     if (err) return res.json({ error: err.sqlMessage });
@@ -76,9 +75,9 @@ app.post('/products01', (req, res) => {
   });
 });
 
-app.get('/products01/:productId', (req, res) => {
+app.get("/products01/:productId", (req, res) => {
   const id = req.params.productId;
-  const q = 'SELECT * FROM product where productId=?';
+  const q = "SELECT * FROM product where productId=?";
   db.query(q, [id], (err, data) => {
     console.log(err, data);
     if (err) return res.json({ error: err.sqlMessage });
@@ -86,17 +85,18 @@ app.get('/products01/:productId', (req, res) => {
   });
 });
 
-app.put('/products01/:productId', (req, res) => {
+app.put("/products01/:productId", (req, res) => {
   const id = req.params.productId;
-  console.log('updated ' + req.body);
+  console.log("updated " + req.body);
   const data = req.body;
   if (data.productPrice) data.productPrice = Number.parseInt(data.productPrice);
-  if (data.availableQuantity) data.availableQuantity = Number.parseInt(data.availableQuantity);
+  if (data.availableQuantity)
+    data.availableQuantity = Number.parseInt(data.availableQuantity);
   const q =
-    'update product set ' +
+    "update product set " +
     Object.keys(data)
       .map((k) => `${k} = ?`)
-      .join(',') +
+      .join(",") +
     " where productId='" +
     id +
     "'";
@@ -110,9 +110,9 @@ app.put('/products01/:productId', (req, res) => {
   });
 });
 
-app.delete('/products01/:productId', (req, res) => {
+app.delete("/products01/:productId", (req, res) => {
   const id = req.params.productId;
-  console.log('deleting ' + id, req.body);
+  console.log("deleting " + id, req.body);
   const { productThumbnail } = req.body;
   console.log(req.body);
   const q = `DELETE FROM product WHERE productId= ?`;
@@ -126,4 +126,3 @@ app.delete('/products01/:productId', (req, res) => {
 app.listen(8082, () => {
   console.log(`listening on port ${8082}`);
 });
-
--- terraform/script/backend.sh
+++ terraform/script/backend.sh
@@ -5,3 +5,11 @@ sudo apt-get update
 
 # Restart ssm agent
 sudo systemctl restart snap.amazon-ssm-agent.amazon-ssm-agent.service
+
+# Install node 20
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install 20
+    nvm use 20
+    nvm alias default 20
